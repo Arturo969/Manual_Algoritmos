e869df9e02287644aa847596d5a765d66504d987
Arturo969 <jesusarturo965@gmail.com>
2024-02-10 00:50:24 -0500

    Se agrega Manual

---
+++ Unidad1/arreglosUnidimensionales/CrearArreglo.java
@@ -0,0 +1,15 @@
+package arreglosUnidimensionales;
+
+public class CrearArreglo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arregloE = new int[10];
+        arregloE[0] = 5;
+        arregloE[1] = 6;
+        arregloE[2] = 8;
+        
+        System.out.println(arregloE[0]);
+	}
+
+}
